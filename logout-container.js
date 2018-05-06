23c126fe051dc85323bb56e396fbb2a1766432f8
seufster <seufster@gmail.com>
2018-05-06 14:50:59 -0400

    update

---
+++ logout-container.js
@@ -0,0 +1,50 @@
+/*
+* Author Edward Seufert
+*/
+'use-strict';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { withRouter } from "react-router";
+import {bindActionCreators} from 'redux';
+import * as userManagementActions from '../../core/usermanagement/usermanagement-actions';
+import fuLogger from '../../core/common/fu-logger';
+import LogoutView from '../../memberView/logout/logout-view';
+
+class LogoutContainer extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	componentDidMount() {
+		let r = this.props.actions.logout();
+		r.then( () => {
+			this.props.history.replace("/");
+		});
+	}
+
+  render() {
+			fuLogger.log({level:'TRACE',loc:'LogoutContainer::render',msg:"Logging out"});
+      return (
+				<LogoutView/>
+			);
+  }
+}
+
+LogoutContainer.propTypes = {
+	appPrefs: PropTypes.object,
+	lang: PropTypes.string,
+	history: PropTypes.object,
+	actions: PropTypes.object
+};
+
+function mapStateToProps(state, ownProps) {
+  return {lang:state.lang, appPrefs:state.appPrefs};
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions:bindActionCreators(userManagementActions,dispatch) };
+}
+
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(LogoutContainer));

15df9797e6af543b9ac6e9d71f017022e6a41442
Edward Seufert <seufster@gmail.com>
2022-01-31 21:33:36 -0500

    Convert to hooks

--- logout-container.js
+++ logout-container.js
@@ -2,47 +2,31 @@
 * Author Edward Seufert
 */
 'use-strict';
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import { withRouter } from "react-router";
-import {bindActionCreators} from 'redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import * as userManagementActions from '../../core/usermgnt/usermgnt-actions';
 import fuLogger from '../../core/common/fu-logger';
 import LogoutView from '../../memberView/logout/logout-view';
 
-class LogoutContainer extends Component {
-	constructor(props) {
-		super(props);
+function LogoutContainer() {
+	const session = useSelector((state) => state.session);
+	const appMenus = useSelector((state) => state.appMenus);
+	const appPrefs = useSelector((state) => state.appPrefs);
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const navigate = useNavigate();
+	
+	fuLogger.log({level:'TRACE',loc:'LogoutContainer::mount',msg:"Logging out"});
+	useEffect(() => {
+		dispatch(userManagementActions.logout());
+	}, []);
 
-	}
-
-	componentDidMount() {
-		fuLogger.log({level:'TRACE',loc:'LogoutContainer::mount',msg:"Logging out"});
-		this.props.actions.logout();
-	}
-
-  render() {
-			fuLogger.log({level:'TRACE',loc:'LogoutContainer::render',msg:"Logging out"});
-      return (
-				<LogoutView/>
-			);
-  }
+    return (
+		<LogoutView/>
+	);
+ 
 }
 
-LogoutContainer.propTypes = {
-	appPrefs: PropTypes.object,
-	lang: PropTypes.string,
-	history: PropTypes.object,
-	actions: PropTypes.object
-};
 
-function mapStateToProps(state, ownProps) {
-  return {lang:state.lang, appPrefs:state.appPrefs};
-}
-
-function mapDispatchToProps(dispatch) {
-  return { actions:bindActionCreators(userManagementActions,dispatch) };
-}
-
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(LogoutContainer));
+export default LogoutContainer;

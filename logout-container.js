8c48aea5097db7455e465a56dd811d51be20eca5
Edward Seufert <seufster@gmail.com>
2022-02-03 20:15:24 -0500

    update to hooks

--- logout-container.js
+++ logout-container.js
@@ -4,14 +4,13 @@
 'use-strict';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import * as userManagementActions from '../../core/usermgnt/usermgnt-actions';
+import { useNavigate, useLocation } from "react-router-dom";
+import * as actions from '../../core/usermgnt/usermgnt-actions';
 import fuLogger from '../../core/common/fu-logger';
 import LogoutView from '../../memberView/logout/logout-view';
 
 function LogoutContainer() {
 	const session = useSelector((state) => state.session);
-	const appMenus = useSelector((state) => state.appMenus);
 	const appPrefs = useSelector((state) => state.appPrefs);
 	const dispatch = useDispatch();
 	const location = useLocation();
@@ -19,7 +18,7 @@ function LogoutContainer() {
 	
 	fuLogger.log({level:'TRACE',loc:'LogoutContainer::mount',msg:"Logging out"});
 	useEffect(() => {
-		dispatch(userManagementActions.logout());
+		dispatch(actions.logout());
 	}, []);
 
     return (

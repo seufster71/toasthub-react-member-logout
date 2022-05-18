b5f9819db9a6711f6373272cc268278274bd6b5a
seufster71 <seufster@gmail.com>
2022-05-18 16:35:44 -0400

    more setup

--- logout-container.js
+++ logout-container.js
@@ -4,7 +4,6 @@
 'use-strict';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate, useLocation } from "react-router-dom";
 import * as actions from '../../core/usermgnt/usermgnt-actions';
 import fuLogger from '../../core/common/fu-logger';
 import LogoutView from '../../memberView/logout/logout-view';
@@ -13,8 +12,6 @@ function LogoutContainer() {
 	const session = useSelector((state) => state.session);
 	const appPrefs = useSelector((state) => state.appPrefs);
 	const dispatch = useDispatch();
-	const location = useLocation();
-	const navigate = useNavigate();
 	
 	fuLogger.log({level:'TRACE',loc:'LogoutContainer::mount',msg:"Logging out"});
 	useEffect(() => {
